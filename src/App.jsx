d9a88a198303ed7b9b9a05a0095df3328e106882
sudeep <sudipdholariya8@gmail.com>
2024-03-13 15:09:37 +0530

    First commit for frontend

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import axios from 'axios';
+function App() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+
+    axios.get('https://fullstack-fawn-nine.vercel.app/api/jokes')
+      .then(function (response) {
+        // handle success
+        setData(response.data);
+        console.log("got",response.data);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+
+  })
+
+  return (
+    <>
+      <h2>This is fontend + Backend</h2>
+      {
+        data.map((item, index) =>
+        (
+          <div key={item.id}>
+            <h3>{item.id}</h3>
+            <h3>{item.joke}</h3>
+            <h3>{item.auther}</h3>
+          </div>
+        )
+
+        )
+      }
+
+    </>
+  )
+}
+
+export default App
